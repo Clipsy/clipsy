63e8f98826fcb920c283c327f772746e66ea3821
Ankit Gupta <ankit.84@gmail.com>
2013-11-14 19:26:00 +0530

    Adding config and addUser method.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 var MongoClient = require('mongodb').MongoClient
+var config = require('../config')
 
 var createId = function(collection, callback){
 	var userid = Math.round(Math.random() * 100000000);
-	collection.find({userid: userid}, function(err, results) {
-         if (!err && results.length == 0) {
+	collection.findOne({userid: userid}, function(user) {
+         if (user == null) {
          	collection.save({userid: userid}, function(err, count){
          		if(!err) {
          			console.log('Userid: ' + userid + 'saved successfully');
@@ -11,20 +12,20 @@ var createId = function(collection, callback){
          		}
          	});
          } else {
-         	console.log(results.length);
          	createId(collection, callback);
          }
-                
 	});
 }
 
 exports.adduser = function(req, res){
-  	MongoClient.connect('mongodb://127.0.0.1:27017/clipsy', function(err, db) {
+  	MongoClient.connect(config.MONGO_URL, function(err, db) {
 		if(err) throw err;
 		var collection = db.collection('users');
         var found = true;
         createId(collection, function(userid){
+        	db.close();
         	res.render('index', { title: userid });
         });
 	});
 };
+
